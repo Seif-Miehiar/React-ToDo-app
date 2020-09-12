bc9170b72d0be48c19e3bbc1991f1ffa93105ada
Seif-Miehiar <Seif.Miehiar@gmail.com>
2020-09-12 22:36:43 +0300

    init

--- my-app/src/components/todo.component.js
+++ my-app/src/components/todo.component.js
@@ -1,49 +1,20 @@
-import React, { useState } from "react";
-// import ReactDOM from "react-dom";
-import TodoList from "./todoList.component"
-// import ls from 'local-storage'
+import React from "react";
+import "./todo.css";
 
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
 
- function Todo()  {
-
-    const [todoItems, setTodoItems] = useState('')
-    const [data, setData] = useState([])
-
-  const handleChange = (event) => {
-    const value = event.target.value
-    setTodoItems(value);
-
- }
-  const handleSubmit = (event) => {
-
-    event.preventDefault()
-    
-    console.log('A name was submitted: ' + todoItems)
-    data.push(todoItems);
-    console.log(data)
-    setTodoItems('')
-  }
-  
-
-      return (
-        <div>
-        Welcome to Todo Component
-        <form onSubmit={handleSubmit}>
-          <label>What are you up to?</label>
-          <input
-          type="text"
-          value={todoItems}
-          onChange={handleChange}
-          />
-          <button type="submit" value="Submit">
-            Add 
-          </button>
-        </form>
-        <TodoList todoItems={data} />
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>x</button>
       </div>
-    )
-  
-  
+    </div>
+  );
 }
 
 export default Todo;
---
+++ my-app/src/components/todoForm.component.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+ function TodoForm({addTodo}) {
+
+  const [value, setValue] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+      return (
+        <div>
+        <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+      </form>
+      </div>
+    )
+  
+  
+}
+
+export default TodoForm;
